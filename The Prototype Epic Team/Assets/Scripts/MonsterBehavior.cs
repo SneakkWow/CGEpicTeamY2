880107ec1c266a0bb07544f8bc8e89852d692009
SneakkWow <logan.c.turney@outlook.com>
2024-09-19 16:26:27 -0500

    monster behavior

---
+++ The Prototype Epic Team/Assets/Scripts/MonsterBehavior.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MonsterBehavior : MonoBehaviour
+{
+    public GameObject target;
+    public NavMeshAgent agent;
+
+    //public Transform goal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        target = GameObject.FindGameObjectWithTag("Player");
+        //agent.destination = goal.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(target.transform.position);
+    }
+    void Wander()
+    {
+       
+    }
+
+}
