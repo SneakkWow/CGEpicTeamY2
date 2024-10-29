81e64bee402a86a08049066db9ad852c16a2711c
scottabbinanti4 <scottabbinanti1@gmail.com>
2024-10-29 16:18:45 -0500

    Stuff

---
+++ EpicTeamProject2/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,35 @@
+//Scott Abbinanti
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] spawnPoints;
+
+    public GameObject[] enemies;
+
+    public static int roundNumber = 1;
+
+    public bool gameOver = false;
+    public bool inRound = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Spawn()
+    {
+
+    }
+}
