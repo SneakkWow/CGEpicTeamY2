3c1a178876c8644b78dd1e845c1d7b6da48e4673
Kstanfor <bluffsummer@gmail.com>
2024-11-26 15:16:19 -0600

    Prefabs and weapon pick up added

--- EpicTeamProject2/Assets/Scripts/WeaponPickUp.cs
+++ EpicTeamProject2/Assets/Scripts/WeaponPickUp.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class WeaponPickUp : MonoBehaviour
 {
+    public string weaponName;
+    public GameObject weaponPrefab;
+
+    private bool isPlayerNearby = false;
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerNearby = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("PLayer"))
+        {
+            isPlayerNearby = false;
+        }
+    }
+
+
     // Update is called once per frame
     void Update()
     {
-        
+       if (isPlayerNearby && Input.GetKeyDown(KeyCode.E))
+        {
+            PickUp();
+        } 
+    }
+
+    void PickUp()
+    {
+        Destroy(gameObject);
     }
 }
